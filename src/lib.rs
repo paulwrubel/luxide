0cc5f4f8c576ac8bbc4649d46809d47fa9b3e54c
Paul Wrubel <pwrubel7@gmail.com>
2023-11-19 03:27:58 -0600

    Add painfully overloaded Vector3 type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::Write};
+use auto_ops::*;
+use std::{
+    fs::File,
+    io::Write,
+    ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
+};
 
 #[derive(Debug, Clone, Copy)]
 struct Color {
@@ -49,3 +54,296 @@ impl Image {
         Ok(())
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector3 {
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    fn length(&self) -> f64 {
+        self.squared_length().sqrt()
+    }
+
+    fn squared_length(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    fn normalize(&mut self) {
+        *self /= self.length();
+    }
+
+    fn as_unit_vector(&self) -> Self {
+        self / self.length()
+    }
+
+    fn dot(&self, other: &Self) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn cross(&self, other: &Self) -> Self {
+        Self {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+impl_op_ex!(+ |a: &Vector3, b: &Vector3| -> Vector3 {
+    Vector3 {
+        x: a.x + b.x,
+        y: a.y + b.y,
+        z: a.z + b.z,
+    }
+});
+
+impl_op_ex!(+= |a: &mut Vector3, b: &Vector3| {
+    a.x += b.x;
+    a.y += b.y;
+    a.z += b.z;
+});
+
+impl_op_ex!(-|a: &Vector3, b: &Vector3| -> Vector3 {
+    Vector3 {
+        x: a.x - b.x,
+        y: a.y - b.y,
+        z: a.z - b.z,
+    }
+});
+
+impl_op_ex!(-= |a: &mut Vector3, b: &Vector3|  {
+    a.x -= b.x;
+    a.y -= b.y;
+    a.z -= b.z;
+});
+
+impl_op_ex!(*|a: &Vector3, b: &Vector3| -> Vector3 {
+    Vector3 {
+        x: a.x * b.x,
+        y: a.y * b.y,
+        z: a.z * b.z,
+    }
+});
+
+impl_op_ex_commutative!(*|a: &Vector3, b: &f64| -> Vector3 {
+    Vector3 {
+        x: a.x * b,
+        y: a.y * b,
+        z: a.z * b,
+    }
+});
+
+impl_op_ex!(*= |a: &mut Vector3, b: &Vector3| {
+    a.x *= b.x;
+    a.y *= b.y;
+    a.z *= b.z;
+});
+
+impl_op_ex!(*= |a: &mut Vector3, b: &f64| {
+    a.x *= b;
+    a.y *= b;
+    a.z *= b;
+});
+
+impl_op_ex!(/|a: &Vector3, b: &Vector3| -> Vector3 {
+    Vector3 {
+        x: a.x / b.x,
+        y: a.y / b.y,
+        z: a.z / b.z,
+    }
+});
+
+impl_op_ex_commutative!(/|a: &Vector3, b: &f64| -> Vector3 {
+    Vector3 {
+        x: a.x / b,
+        y: a.y / b,
+        z: a.z / b,
+    }
+});
+
+impl_op_ex!(/= |a: &mut Vector3, b: &Vector3| {
+    a.x /= b.x;
+    a.y /= b.y;
+    a.z /= b.z;
+});
+
+impl_op_ex!(/= |a: &mut Vector3, b: &f64| {
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+});
+
+impl Index<usize> for Vector3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Index out of bounds"),
+        }
+    }
+}
+
+impl IndexMut<usize> for Vector3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => panic!("Index out of bounds"),
+        }
+    }
+}
+
+// impl Add for Vector3 {
+//     type Output = Self;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         Self {
+//             x: self.x + rhs.x,
+//             y: self.y + rhs.y,
+//             z: self.z + rhs.z,
+//         }
+//     }
+// }
+
+// impl AddAssign for Vector3 {
+//     fn add_assign(&mut self, rhs: Self) {
+//         self.x += rhs.x;
+//         self.y += rhs.y;
+//         self.z += rhs.z;
+//     }
+// }
+
+// impl Sub for Vector3 {
+//     type Output = Self;
+
+//     fn sub(self, rhs: Self) -> Self::Output {
+//         Self {
+//             x: self.x - rhs.x,
+//             y: self.y - rhs.y,
+//             z: self.z - rhs.z,
+//         }
+//     }
+// }
+
+// impl SubAssign for Vector3 {
+//     fn sub_assign(&mut self, rhs: Self) {
+//         self.x -= rhs.x;
+//         self.y -= rhs.y;
+//         self.z -= rhs.z;
+//     }
+// }
+
+// impl Mul for Vector3 {
+//     type Output = Self;
+
+//     fn mul(self, rhs: Self) -> Self::Output {
+//         Self {
+//             x: self.x * rhs.x,
+//             y: self.y * rhs.y,
+//             z: self.z * rhs.z,
+//         }
+//     }
+// }
+
+// impl Mul<f64> for Vector3 {
+//     type Output = Self;
+
+//     fn mul(self, rhs: f64) -> Self::Output {
+//         Self {
+//             x: self.x * rhs,
+//             y: self.y * rhs,
+//             z: self.z * rhs,
+//         }
+//     }
+// }
+
+// impl Mul<Vector3> for f64 {
+//     type Output = Vector3;
+
+//     fn mul(self, rhs: Vector3) -> Self::Output {
+//         Vector3 {
+//             x: self * rhs.x,
+//             y: self * rhs.y,
+//             z: self * rhs.z,
+//         }
+//     }
+// }
+
+// impl MulAssign for Vector3 {
+//     fn mul_assign(&mut self, rhs: Self) {
+//         self.x *= rhs.x;
+//         self.y *= rhs.y;
+//         self.z *= rhs.z;
+//     }
+// }
+
+// impl MulAssign<f64> for Vector3 {
+//     fn mul_assign(&mut self, rhs: f64) {
+//         self.x *= rhs;
+//         self.y *= rhs;
+//         self.z *= rhs;
+//     }
+// }
+
+// impl Div for Vector3 {
+//     type Output = Self;
+
+//     fn div(self, rhs: Self) -> Self::Output {
+//         Self {
+//             x: self.x / rhs.x,
+//             y: self.y / rhs.y,
+//             z: self.z / rhs.z,
+//         }
+//     }
+// }
+
+// // impl Div for &Vector3 {
+// //     type Output = Vector3;
+
+// //     fn div(self, rhs: Self) -> Self::Output {
+// //         Vector3 {
+// //             x: self.x / rhs.x,
+// //             y: self.y / rhs.y,
+// //             z: self.z / rhs.z,
+// //         }
+// //     }
+// // }
+
+// impl Div<f64> for Vector3 {
+//     type Output = Self;
+
+//     fn div(self, rhs: f64) -> Self::Output {
+//         Self {
+//             x: self.x / rhs,
+//             y: self.y / rhs,
+//             z: self.z / rhs,
+//         }
+//     }
+// }
+
+// impl DivAssign for Vector3 {
+//     fn div_assign(&mut self, rhs: Self) {
+//         self.x /= rhs.x;
+//         self.y /= rhs.y;
+//         self.z /= rhs.z;
+//     }
+// }
+
+// impl DivAssign<f64> for Vector3 {
+//     fn div_assign(&mut self, rhs: f64) {
+//         self.x /= rhs;
+//         self.y /= rhs;
+//         self.z /= rhs;
+//     }
+// }
