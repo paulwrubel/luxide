e7e07a42d55f91f0601564c6bead210e383097bf
Paul Wrubel <pwrubel7@gmail.com>
2025-05-06 00:19:10 -0500

    Add smui

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import IconButton, { Icon } from '@smui/icon-button';
+	import { mdiInformation } from '@mdi/js';
+</script>
+
+<TopAppBar variant="static">
+	<Row>
+		<Section>
+			<Title>Luxide</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton>
+				<Icon tag="svg" viewBox="0 0 24 24">
+					<path fill="currentColor" d={mdiInformation} />
+				</Icon>
+			</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<main class="main-content">
+	<h1>Welcome to Luxide</h1>
+	<p>A path tracer written in Rust</p>
+</main>
+
+<style>
+	.main-content {
+		margin: 1rem;
+	}
+
+	:global(.mdc-top-app-bar) {
+		background-color: #1976d2;
+	}
+</style>
