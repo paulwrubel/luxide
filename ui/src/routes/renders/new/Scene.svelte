751381c652978f84975d61c598d970b6f7e55f5d
Paul Wrubel <pwrubel7@gmail.com>
2025-05-12 23:05:04 -0500

    Add more scene elements

--- ui/src/routes/renders/new/Scene.svelte
+++ ui/src/routes/renders/new/Scene.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { toRadians } from '$lib/math';
 	import { T } from '@threlte/core';
+	import { DoubleSide, FrontSide } from 'three';
 </script>
 
 <T.PerspectiveCamera
@@ -11,13 +13,50 @@
 	}}
 />
 
-<!-- subject sphere -->
-<T.Mesh position={[0.5, 0.5, -0.5]}>
-	<T.SphereGeometry args={[0.1]} />
-	<T.MeshBasicMaterial color="green" />
+<T.Mesh position={[0.1, 0.1, -0.1]}>
+	<T.SphereGeometry args={[0.05]} />
+	<T.MeshLambertMaterial color="green" emissive="white" />
+</T.Mesh>
+<T.PointLight position={[0.1, 0.1, -0.1]} castShadow receiveShadow />
+
+<!-- sphere -->
+<T.Mesh position={[0.5, 0.5, -0.5]} castShadow receiveShadow>
+	<T.SphereGeometry args={[0.25]} />
+	<T.MeshStandardMaterial color="green" />
 </T.Mesh>
 
-<T.Mesh position={[0, 0.5, -0.5]} rotation={[88, 0, 0]}>
+<!-- left wall -->
+<T.Mesh position={[0, 0.5, -0.5]} rotation={[0, toRadians(90), 0]} castShadow receiveShadow>
 	<T.PlaneGeometry args={[1, 1]} />
-	<T.MeshBasicMaterial color="red" />
+	<T.MeshStandardMaterial color="red" />
+</T.Mesh>
+
+<!-- right wall -->
+<T.Mesh position={[1, 0.5, -0.5]} rotation={[0, toRadians(-90), 0]} castShadow receiveShadow>
+	<T.PlaneGeometry args={[1, 1]} />
+	<T.MeshStandardMaterial color="blue" />
+</T.Mesh>
+
+<!-- floor -->
+<T.Mesh position={[0.5, 0, -0.5]} rotation={[toRadians(-90), 0, 0]} castShadow receiveShadow>
+	<T.PlaneGeometry args={[1, 1]} />
+	<T.MeshStandardMaterial color="green" />
+</T.Mesh>
+
+<!-- ceiling -->
+<T.Mesh position={[0.5, 1, -0.5]} rotation={[toRadians(90), 0, 0]} castShadow receiveShadow>
+	<T.PlaneGeometry args={[1, 1]} />
+	<T.MeshStandardMaterial color="purple" />
+</T.Mesh>
+
+<!-- back wall -->
+<T.Mesh position={[0.5, 0.5, -1.0]} rotation={[0, 0, 0]} castShadow receiveShadow>
+	<T.PlaneGeometry args={[1, 1]} />
+	<T.MeshStandardMaterial color="white" />
+</T.Mesh>
+
+<!-- front wall -->
+<T.Mesh position={[0.5, 0.5, 0]} rotation={[toRadians(180), 0, 0]} castShadow receiveShadow>
+	<T.PlaneGeometry args={[1, 1]} />
+	<T.MeshStandardMaterial color="white" />
 </T.Mesh>
