29f7a04b2058f57516422fd78539d3ef0e8634b7
Paul LeClair <pwrubel7@gmail.com>
2026-06-07 19:23:52 -0500

    Fix the formatting of the variant controls

--- ui/src/views/renders/new/Controls/cards/ControlsCardGeometric/AroundVariantControls.tsx
+++ ui/src/views/renders/new/Controls/cards/ControlsCardGeometric/AroundVariantControls.tsx
@@ -1,7 +1,9 @@
+import { ExpandableSection } from '@/components/ExpandableSection';
 import { TextArrayInputControl } from '@/components/form-controls/TextArrayInputControl';
 import { isAroundPoint } from '@/utils/render/utils';
 import type { Around } from '@/utils/render/utils';
 import type { RenderForm } from '@/hooks/useRenderForm';
+import { AnimatedSeparator } from '@/components/AnimatedSeparator';
 
 export type AroundVariantControlsProps = {
   form: RenderForm;
@@ -13,42 +15,55 @@ export function AroundVariantControls(props: AroundVariantControlsProps) {
 
   return (
     <form.AppField name={`geometrics.${geometricName}.around`}>
-      {(field) => (
-        <>
-          <field.SelectControl
-            label="Rotation Point"
-            items={[
-              { label: 'Geometric Center', value: 'center' },
-              { label: 'World Origin', value: 'origin' },
-              { label: 'Custom Point', value: 'point' },
-            ]}
-            onChange={(value) => {
-              if (value === 'center' || value === 'origin') {
-                // superRefine schema prevents TS from narrowing the discriminated union here
-                field.handleChange(value as never);
-              } else {
-                // superRefine schema prevents TS from narrowing the discriminated union here
-                field.handleChange({ point: [0, 0, 0] } as never);
-              }
-            }}
-            mapValue={(v) => {
-              if (typeof v === 'string') {
-                return v;
-              }
-              return 'point';
-            }}
-          />
-          {isAroundPoint((field.state.value as Around | undefined) ?? 'origin') && (
-            <TextArrayInputControl
-              form={form}
-              fieldName={`geometrics.${geometricName}.around.point`}
-              label="Custom Rotation Point"
-              valueLabels={['x', 'y', 'z']}
-              type="number"
+      {(field) => {
+        const isPoint = isAroundPoint((field.state.value as Around | undefined) ?? 'origin');
+
+        return (
+          <>
+            <AnimatedSeparator visible={isPoint} />
+            <field.SelectControl
+              label="Rotation Point"
+              items={[
+                { label: 'Geometric Center', value: 'center' },
+                { label: 'World Origin', value: 'origin' },
+                { label: 'Custom Point', value: 'point' },
+              ]}
+              onChange={(value) => {
+                if (value === 'center' || value === 'origin') {
+                  // superRefine schema prevents TS from narrowing the discriminated union here
+                  field.handleChange(value as never);
+                } else {
+                  // superRefine schema prevents TS from narrowing the discriminated union here
+                  field.handleChange({ point: [0, 0, 0] } as never);
+                }
+              }}
+              mapValue={(v) => {
+                if (typeof v === 'string') {
+                  return v;
+                }
+                return 'point';
+              }}
             />
-          )}
-        </>
-      )}
+            <ExpandableSection
+              expanded={isPoint}
+              onExpandEnd={() => {
+                form.validate('change');
+              }}
+            >
+              <div className="py-2">
+                <TextArrayInputControl
+                  form={form}
+                  fieldName={`geometrics.${geometricName}.around.point`}
+                  label="Custom Rotation Point"
+                  valueLabels={['x', 'y', 'z']}
+                  type="number"
+                />
+              </div>
+            </ExpandableSection>
+            <AnimatedSeparator visible={isPoint} />
+          </>
+        );
+      }}
     </form.AppField>
   );
 }
